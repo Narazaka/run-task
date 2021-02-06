0ad3332db90fdcb54647bc5d8a63d09ff87b3f64
Narazaka <info@narazaka.net>
2021-02-06 21:04:20 +0900

    少し丁寧にした

--- run-task.c
+++ run-task.c
@@ -1,11 +1,16 @@
 #include <string.h>
 #include <stdlib.h>
 
+const char* cmd = "C:\\Windows\\System32\\schtasks.exe /RUN /TN \"";
+
 int main(int argc, char** argv) {
     size_t len = strlen(argv[0]);
     argv[0][len - 4] = '"';
     argv[0][len - 3] = '\0';
-    char* cmd = "C:\\Windows\\System32\\schtasks.exe /RUN /TN \"";
-    strcat(cmd, argv[0]);
-    return system(cmd);
+    char* execcmd = malloc(sizeof(char*) * (strlen(cmd) + len - 2));
+    strcpy(execcmd, cmd);
+    strcat(execcmd, argv[0]);
+    int result = system(execcmd);
+    free(execcmd);
+    return result;
 }
